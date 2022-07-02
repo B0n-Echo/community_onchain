f1cbc715e123734c522d13ff92583acfdec41dfe
NikhileshP <nikhileshhpandey@gmail.com>
2022-07-02 12:13:49 +0530

    🚧Adding Authentication routes and components to the application

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Switch ,Route } from "react-router-dom";
 import App from "./App";
+import Signup from "./auth/signup";
+import Signin from "./auth/signin";
+import Activate from "./auth/activate";
 
 const Routes = () => {
     return (
         <BrowserRouter>
         <Switch>
             <Route path="/"  exact component={App} />
+            <Route path="/signup"  exact component={Signup} />
+            <Route path="/signin"  exact component={Signin} />
+            <Route path="auth/activate"  exact component={Activate} />
         </Switch>
         </BrowserRouter>
     )
