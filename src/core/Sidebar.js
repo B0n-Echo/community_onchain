2270b52cf41de721dc0891380bdb764c5b77da84
NikhileshP <nikhileshhpandey@gmail.com>
2022-07-03 14:02:07 +0530

    🚧Adding sidebar changes and sidebar Channel changes

--- src/core/Sidebar.js
+++ src/core/Sidebar.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import "./Sidebar.css"
+import SidebarChannel from './SidebarChannel';
+
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
-    
       <div className="sidebar__top">
         <h3>Community Channel</h3>
+      </div>
 
-        
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <h4>Test here</h4>
+          </div>
+        </div>
+        <div className="sidebar__channelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
       </div>
     </div>
   );
