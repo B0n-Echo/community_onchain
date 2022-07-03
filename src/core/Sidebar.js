0714ad41d3b924924610b61137b27443e70c650b
NikhileshP <nikhileshhpandey@gmail.com>
2022-07-03 14:59:08 +0530

    ✨ Additional changes for sidebarChannels

--- src/core/Sidebar.js
+++ src/core/Sidebar.js
@@ -5,24 +5,49 @@ import SidebarChannel from './SidebarChannel';
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__top">
-        <h3>Community Channel</h3>
+    <div className="sidebar__top">
+      <h3>Community Server</h3>
+    </div>
+
+    <div className="sidebar__channels">
+      <div className="sidebar__channelsHeader">
+        <div className="sidebar__header">
+        <h4>Test here</h4>
+        </div>
+
+                   </div>
+        <div className="sidebar__channelsList">
+             <SidebarChannel />
+             <SidebarChannel />
+              <SidebarChannel />
+              <SidebarChannel />
+        </div>
+    </div>
+
+    <div className="sidebar__voice">
+      <div className="sidebar__voiceInfo">
+        <h3>Voice Connected</h3>
+        <p>Stream</p>
       </div>
 
-      <div className="sidebar__channels">
-        <div className="sidebar__channelsHeader">
-          <div className="sidebar__header">
-            <h4>Test here</h4>
-          </div>
-        </div>
-        <div className="sidebar__channelsList">
-          <SidebarChannel />
-          <SidebarChannel />
-          <SidebarChannel />
-          <SidebarChannel />
-        </div>
+      <div className="sidebar__voiceIcons">
       </div>
     </div>
+
+    <div className="sidebar__profile">
+      {/* <Avatar /> */}
+      <div className="sidebar__profileInfo">
+        <h3>SrikarK</h3>
+        <p>#ID</p>
+      </div>
+
+      <div className="sidebar__profileIcons">
+      🚧
+      🚧
+      🚧
+      </div>
+    </div>
+  </div>
   );
 }
 
--- src/core/SidebarChannel.js
+++ src/core/SidebarChannel.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./SidebarChannel.css"
-function SidebarChannel({ id, channel }) {
+function SidebarChannel({ id, channelName  }) {
   return (
     <div className="sidebarChannel">
       <h4>
-        <span className="sidebarChannel_hash">#</span>Youtube
+        <span className="sidebarChannel_hash">#</span> Youtube
       </h4>
     </div>
   );
